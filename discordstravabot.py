2c65416718d763a63517600ce3dd90f70cc2416b
Robert Game <robert.christopher.game@gmail.com>
2019-06-28 13:53:49 +0100

    Strava API integration added. Basic stat function

--- discordstravabot.py
+++ discordstravabot.py
@@ -4,6 +4,7 @@ Made for Discord Hack Week 2019 '''
 import os
 import sys
 import discord
+import requests
 
 # Grab the Discord bot token from DISCORDTOKEN environment variable
 DISCORDTOKEN = os.environ.get('DISCORDTOKEN')
@@ -22,7 +23,16 @@ if STRAVATOKEN is None:
 else:
     print("Strava token is", STRAVATOKEN)
 
+# Set Strava Club ID
+STRAVACLUB = "531232"
+
+# Building Strava authentication header
+stravaAuthHeader = {'Content-Type': 'application/json',
+                    'Authorization': 'Bearer {}'.format(STRAVATOKEN)}
+
+
 class StravaIntegration(discord.Client):
+
     async def on_ready(self):
         ''' Function fires when bot connects '''
         print('Logged on as', self.user)
@@ -33,8 +43,24 @@ class StravaIntegration(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content == '!leaderboard':
-            await message.channel.send('Showing leaderboard.')
+        if message.content == '!statistics':
+            print("Running Strava API call../")
+
+            stravaResult = requests.get('https://www.strava.com/api/v3/clubs/' +
+                                        STRAVACLUB+'/activities',
+                                        headers=stravaAuthHeader)
+            #print(stravaResult.json())
+            totalDistance = 0
+            totalDistanceThisWeek = 0
+
+            for activity in stravaResult.json():
+                totalDistance += activity['distance']
+            statisticsMsg = 'Together we have run ' + \
+                            str(round(totalDistance/1000, 2)) + \
+                            ' km! That\'s like...really far dude.'
+
+            await message.channel.send(statisticsMsg)
+
 
 client = StravaIntegration()
 client.run(DISCORDTOKEN)
